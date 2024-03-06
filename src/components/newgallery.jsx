1eaac86c25ae6fe1e470443cb82aa2e4013bb181
unknown <uurosrakic@gmail.com>
2024-03-06 20:19:51 +0100

    updated gallery and video

--- src/components/newgallery.jsx
+++ src/components/newgallery.jsx
@@ -3,28 +3,56 @@ import ImageGallery from "react-image-gallery";
 
 const images = [
     {
-        "original": "img/portfolio/01-large.jpg",
-        "thumbnail": "img/portfolio/01-small.jpg"
+        "original": "img/portfolio/1-large.jpg",
+        "thumbnail": "img/portfolio/1-small.jpg"
       },
       {
-        "original": "img/portfolio/02-large.jpg",
-        "thumbnail": "img/portfolio/02-small.jpg"
+        "original": "img/portfolio/2-large.jpg",
+        "thumbnail": "img/portfolio/2-small.jpg"
       },
       {
-        "original": "img/portfolio/03-large.jpg",
-        "thumbnail": "img/portfolio/03-small.jpg"
+        "original": "img/portfolio/3-large.jpg",
+        "thumbnail": "img/portfolio/3-small.jpg"
       },
       {
-        "original": "img/portfolio/04-large.jpg",
-        "thumbnail": "img/portfolio/04-small.jpg"
+        "original": "img/portfolio/4-large.jpg",
+        "thumbnail": "img/portfolio/4-small.jpg"
       },
       {
-        "original": "img/portfolio/05-large.jpg",
-        "thumbnail": "img/portfolio/05-small.jpg"
+        "original": "img/portfolio/5-large.jpg",
+        "thumbnail": "img/portfolio/5-small.jpg"
       },
       {
-        "original": "img/portfolio/06-large.jpg",
-        "thumbnail": "img/portfolio/06-small.jpg"
+        "original": "img/portfolio/6-large.jpg",
+        "thumbnail": "img/portfolio/6-small.jpg"
+      },
+      {
+        "original": "img/portfolio/7-large.jpg",
+        "thumbnail": "img/portfolio/7-small.jpg"
+      },
+      {
+        "original": "img/portfolio/8-large.jpg",
+        "thumbnail": "img/portfolio/8-small.jpg"
+      },
+      {
+        "original": "img/portfolio/9-large.jpg",
+        "thumbnail": "img/portfolio/9-small.jpg"
+      },
+      {
+        "original": "img/portfolio/10-large.jpg",
+        "thumbnail": "img/portfolio/10-small.jpg"
+      },
+      {
+        "original": "img/portfolio/11-large.jpg",
+        "thumbnail": "img/portfolio/11-small.jpg"
+      },
+      {
+        "original": "img/portfolio/12-large.jpg",
+        "thumbnail": "img/portfolio/12-small.jpg"
+      },
+      {
+        "original": "img/portfolio/13-large.jpg",
+        "thumbnail": "img/portfolio/13-small.jpg"
       }
   ];
 
