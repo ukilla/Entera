944376d85a2ee8de0148dc2088f015b54bd2b094
ukilla <uurosrakic@gmail.com>
2024-04-23 12:40:41 +0200

    about addition, glitch fix

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,6 +4,49 @@ export const About = (props) => {
   return (
     <div id="about">
       <div className="first-section">
+        <div className="container web-site-section">
+          <div className="row">
+            <div className="col-xs-12 col-md-6">
+              <div className="about-text">
+                <h2>Izrada web sajtova i animacije</h2>
+                <p>{props.data ? props.data.paragraph3 : "loading..."}</p>
+                <a
+                  href="https://entera-interactive.netlify.app"
+                  className="btn btn-custom btn-lg"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Demo sajt
+                </a>{" "}
+                <h3>Prednosti ove usluge?</h3>
+                <div className="list-style">
+                  <div className="col-lg-6 col-sm-6 col-xs-12">
+                    <ul>
+                      {props.data
+                        ? props.data.Why5.map((d, i) => (
+                            <li key={`${d}-${i}`}>{d}</li>
+                          ))
+                        : "loading"}
+                    </ul>
+                  </div>
+                  <div className="col-lg-6 col-sm-6 col-xs-12">
+                    <ul>
+                      {props.data
+                        ? props.data.Why6.map((d, i) => (
+                            <li key={`${d}-${i}`}> {d}</li>
+                          ))
+                        : "loading"}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="other-section col-xs-12 col-md-6">
+              {" "}
+              <img src="img/site.jpg" className="img-responsive" alt="" />{" "}
+            </div>
+          </div>
+        </div>
         <div className="container">
           <div className="row">
             <div className="col-xs-12 col-md-6">
